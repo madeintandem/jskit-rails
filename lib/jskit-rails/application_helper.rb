82202cc41f6db291299116cd2e0337a8c59979d9
Dayton Nolan <daytonn@gmail.com>
2014-10-01 00:25:20 -0500

    Add controller:all event

--- lib/jskit-rails/application_helper.rb
+++ lib/jskit-rails/application_helper.rb
@@ -1,11 +1,11 @@
 module JSKit
   module ApplicationHelper
     def jskit(config={namespace: nil})
-      namespace = config[:namespace] ? "#{config[:namespace]}:" : ""
       payload = JSKit::Payload.to_args
       JSKit::Payload.value = nil
+      event_name = [config[:namespace], "controller", controller_name, action_name].compact.join(":")
       javascript_tag do
-        "#{JSKit::Config.app_namespace}.Dispatcher.trigger(\"#{namespace}controller:#{controller_name}:#{action_name}\"#{payload});".html_safe
+        "#{JSKit::Config.app_namespace}.Dispatcher.trigger(\"controller:all\");\n#{JSKit::Config.app_namespace}.Dispatcher.trigger(\"#{event_name}\"#{payload});".html_safe
       end
     end
   end
